6c76667020d52fc6c6b4c218e12f1819a12c916a
Bhanu Prakash Paruchuri <bhanu@Bhanus-MacBook-Pro.local>
2018-11-27 23:17:56 -0500

    Interchanged Lines

--- GitProject/src/com/git/test/GitTest.java
+++ GitProject/src/com/git/test/GitTest.java
@@ -3,8 +3,8 @@ package com.git.test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("GIT Test");
 		System.out.println("Line 1");
+		System.out.println("GIT Test");
 
 	}
 
